010c8fa7133e2a76898c9edcf62353d5058d5b7f
Martin Gundtoft <marg@kea.dk>
2023-06-26 22:55:16 +0200

    updated to azure

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === "production") {
+      config.mode = "production";
+      config.resolve.alias["@env"] = require("path").resolve(
+        process.cwd(),
+        ".env.production.local"
+      );
+    } else {
+      config.mode = "development";
+      config.resolve.alias["@env"] = require("path").resolve(
+        process.cwd(),
+        ".env.development.local"
+      );
+    }
+  },
 });
