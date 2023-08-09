5a9d86b5ebdf2e7ab77691e88044862f9bbec0f7
Martin Gundtoft <marg@kea.dk>
2023-08-09 13:03:08 +0200

    simple post system ver 6

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import ProjectView from "../views/ProjectView.vue";
 
@@ -25,8 +25,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  mode: "hash", // Enable hash mode
+  history: createWebHashHistory(process.env.BASE_URL),
   routes,
 });
 
