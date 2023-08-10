2fb35f59918d236c315825035b48dfd0610ea673
Martin Gundtoft <marg@kea.dk>
2023-08-10 13:23:53 +0200

    simple post system ver 9 (loading lottie)

---
+++ src/components/ChatLottieWhite.vue
@@ -0,0 +1,32 @@
+// https://blog.logrocket.com/creating-animated-logos-lottie-vue-3/
+
+<template>
+  <main>
+    <div class="wrapper">
+      <Vue3Lottie :animationData="playJSON" :height="height" :width="width" />
+    </div>
+  </main>
+</template>
+
+<script>
+import playJSON from "../assets/lotties/chatbot-animation_white.json";
+
+export default {
+  props: {
+    width: String,
+    height: String,
+    margin: String,
+  },
+  data() {
+    return {
+      playJSON,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.lottie-animation-container {
+  margin: v-bind(margin);
+}
+</style>
