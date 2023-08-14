4c3aa7f0de9919febe28f03531a68ba8ebac273f
Martin Gundtoft <marg@kea.dk>
2023-08-14 15:49:27 +0200

    added network to nav

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,9 @@
           <img src="@/assets/img/KEA_logo_DK_Web_gai.jpg" width="60" />
         </router-link>
         <router-link to="/network">Survey</router-link>
-        <router-link to="/chatgpt">Prøv GPT4</router-link>
         <router-link to="/liveblog">Liveblog</router-link>
+        <router-link to="/">Netværk</router-link>
+        <router-link to="/chatgpt">GPT4</router-link>
       </div>
     </nav>
     <!-- <div class="wave"></div>
