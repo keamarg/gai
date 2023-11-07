a43673314892107ffe3cf2367eac6aa75cf05e59
Martin Gundtoft <marg@kea.dk>
2023-11-07 16:26:13 +0100

    added news + box

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -2,104 +2,258 @@
   <div class="container">
     <div class="header">
       <img src="@/assets/img/KEA_logo_DK_Web_gai.jpg" class="headerLogo" />
-      <h1>Nyheder</h1>
+      <h1>GAI nyheder</h1>
     </div>
-    <h3><i>24/10 2023</i></h3>
-    <h2>Ny "State of AI" rapport</h2>
-    <p>
-      "State of AI" - en årlig tilstandsrapport der de sidste seks år har
-      kortlagt og vurderet feltet inden for kunstig intelligens, er netop
-      udkommet for 2023. G-AI LAB er dykket ned i rapporten og har her lavet et
-      kort overblik over dens mest interessante emner og tendenser.
-    </p>
-    <div class="imgDiv">
-      <a href="https://www.stateof.ai">
-        <img
-          src="@/assets/img/hal.jpeg"
-          class="newsImg"
-          alt="https://www.stateof.ai"
-        />
-      </a>
+    <div class="innercontainer">
+      <div class="contentbox">
+        <h3><i>7/11 2023</i></h3>
+        <h2>USA indfører omfattende lov om kunstig intelligens</h2>
+        <div class="imgDiv">
+          <a
+            href="https://www.computerworld.dk/art/284890/usa-indfoerer-omfattende-lov-om-kunstig-intelligens-nyt-dekret-skal-regulere-disse-otte-omraader"
+            target="_blank"
+          >
+            <img
+              src="https://www.computerworld.dk/fil/209087"
+              class="newsImg"
+              alt="https://www.computerworld.dk/art/284890/usa-indfoerer-omfattende-lov-om-kunstig-intelligens-nyt-dekret-skal-regulere-disse-otte-omraader"
+            />
+          </a>
+        </div>
+        <p>
+          Joe Biden overhaler EU indenom, og underskriver sit første dekret om
+          kunstig intelligens. Det nye tiltag skal beskytte forbrugere og
+          arbejdstagere og samtidig styrke USA’s konkurrenceevne på området.
+        </p>
+        <a
+          href="https://www.whitehouse.gov/briefing-room/presidential-actions/2023/10/30/executive-order-on-the-safe-secure-and-trustworthy-development-and-use-of-artificial-intelligence/"
+          target="_blank"
+          >Læs dekretet her
+        </a>
+        <a
+          href="https://www.computerworld.dk/art/284890/usa-indfoerer-omfattende-lov-om-kunstig-intelligens-nyt-dekret-skal-regulere-disse-otte-omraader"
+          target="_blank"
+          >Kort sammenfatning fra Computerworld
+        </a>
+        <h3><i>7/11 2023</i></h3>
+        <h2>AI Denmark Summit 2023</h2>
+        <p>
+          Den 26. oktober 2023 blev AI Denmark Summit afholdt, en begivenhed,
+          der behandler brug af kunstig intelligens i danske virsomheder, og
+          diskuterer de nyeste fremskridt og trends indenfor feltet. To
+          repræsentanter fra KEAs FKR afdeling var til stede for at følge
+          begivenheden, som blev faciliteret af Anders Høegh Nissen.
+        </p>
+        <div class="imgDiv">
+          <a href="https://aidenmark.dk/" target="_blank">
+            <img
+              src="https://media.licdn.com/dms/image/D4E22AQEIrojfcVXHEQ/feedshare-shrink_1280/0/1696927146312?e=1702512000&v=beta&t=Fj3sMtRAB14f4hxD814rpNzRzaisGk3BgYZBl2QIPPI"
+              class="newsImg"
+              alt="https://aidenmark.dk/"
+            />
+          </a>
+        </div>
+        <p>
+          AI Denmark er finansieret af industriens fond, og er til for at hjælpe
+          SMV'er med at komme i gang med brugen af AI-drevne løsninger. Efter en
+          kort rammesætning og introduktion fra Anders Høegh Nissen (Tech
+          journalist) og Thomas Hofman-Bang (CEO i Industriens Fond) om status
+          på brugen af AI i danske SMV'ere gik Juan Ferré (CEO for Teknologisk
+          Institut) på scenen, og introducerede konceptet “supercharged” - AI
+          som en måde at gøre det vi allerede er gode til meget bedre; ikke en
+          afløser for menneskelig arbejdskraft og kreativitet. Han henviste også
+          til en Forbes-rapport om AI, hvor han bemærkede, at der er en tendens
+          til at overvurdere korttidseffekterne af nye teknologier som AI, mens
+          langtidseffekterne ofte undervurderes.
+        </p>
+        <p>
+          Derefter demonstrerede en række danske SMV'ere, hvordan de bruger AI
+          til at forbedre deres drift og produkter, bl.a. Donkey Republic, som
+          delte deres udfordringer med at rebalancere - det vil sige at få
+          cyklerne derhen, hvor de gerne vil have dem; Farmdroid, som fortalte
+          om deres bæredygtige maskiner til landbruget, der skal passe
+          afgrøderne fra såning til høst; RD8 om mekanisk produktudvikling;
+          MyDefence om anti drone systemer, og en asfaltproducent, der bruger AI
+          til at vælge de rette asfalttyper til de rigtige steder. Desuden blev
+          der undervejs givet en række andre gode eksempler: Optimum Voyage, som
+          bruger AI til at spare brændstof; Daloon, der anvender AI til
+          kvalitetssikring i produktion af fødevarer og Norsk Laks, der bruger
+          AI til at finde de bedste avlsdyr, så de undgår at slagte de
+          eksemplarer, der bør bruges til avl.
+        </p>
+        <p>
+          Peter Lemcke fra AI Denmark fortalte mere om danske SMV'er og deres
+          brug af AI - bl.a. nævnte han, at kun ca. 15% bruger det pt. i større
+          eller mindre grad. Jan Damsgaard fortalte om ATVs faglige dataetiske
+          råd, om nye retningslinjer for uddannelse om AI der er på trapperne,
+          og om hans nye bog "AI mellem fornuft og følelse". Reza Zadeh fra
+          Stanford/Matroid og Ole Winter fra DTU leverede også inspirerende
+          præsentationer med et mere teknisk fokus, mens Mette Kaagard fra
+          Microsoft fortalte om Microsofts strategi og deres nye Copilot
+          satsning.
+        </p>
+        <p>
+          AI Denmark Summit 2023 var en god oplevelse, fyldt med gode
+          diskussioner og værdifulde indsigter i AI's indvirkning på både små og
+          store virksomheder. Særligt var det spændende at høre lidt om konkret
+          brug af AI teknologi i de danske SMV'er.
+        </p>
+
+        <br />
+
+        <a href="https://aidenmark.dk/" target="_blank">Link til AI Denmark</a>
+        <a
+          href="https://atv.dk/temaer-projekter/projekt/atvs-faglige-dataetiske-raad"
+          target="_blank"
+          >Link til ATVs faglige dataetiske råd</a
+        >
+        <h3><i>24/10 2023</i></h3>
+        <h2>Ny "State of AI" rapport</h2>
+        <p>
+          "State of AI" - en årlig tilstandsrapport der de sidste seks år har
+          kortlagt og vurderet feltet inden for kunstig intelligens, er netop
+          udkommet for 2023. G-AI LAB er dykket ned i rapporten og har her lavet
+          et kort overblik over dens mest interessante emner og tendenser.
+        </p>
+        <div class="imgDiv">
+          <a href="https://www.stateof.ai" target="_blank">
+            <img
+              src="@/assets/img/hal.jpeg"
+              class="newsImg"
+              alt="https://www.stateof.ai"
+            />
+          </a>
+        </div>
+
+        <ol>
+          <li>
+            <strong>Hvordan GPT-4 dominerer AI-landskabet.</strong>OpenAI's
+            GPT-4 model, grundlaget for ChatGPT, har været i fokus i det seneste
+            år. GPT-4 er anerkendt som den mest modne og kraftfulde store
+            sprogmodel på markedet. Den har demonstreret evnen til at bestå
+            universitetsniveau-eksaminer inden for fag som jura og medicin ud
+            over klassiske NLP-benchmarks.
+          </li>
+          <li>
+            <strong
+              >Hvordan RLHF har bevist sit værd og hvorfor det alligevel
+              kritiseres kraftigt.</strong
+            >
+            GPT-4 er trænet ved hjælp af "reinforcement learning from human
+            feedback" (RLHF), hvor menneskelige testere vurderer
+            modelresultater. RLHF er afgørende for præstationen af brugervenlige
+            chatbots som ChatGPT. Kritik er rejst angående de økonomiske og
+            menneskelige omkostninger ved RLHF og beskæftigelsen af mange
+            testere./li>
+          </li>
+
+          <li>
+            <strong
+              >Hvordan en ny tidsånd af mindre vidensdeling og gennemsigtighed
+              vandt frem.</strong
+            >
+            Tidligere var offentliggørelse af forskningsresultater kutyme, men
+            store kommercielle AI-selskaber som OpenAI og Google er blevet mere
+            lukkede. Tekniske rapporter om ny AI-teknologi indeholder færre
+            tekniske detaljer og øger hemmeligholdelsen. Lukkethed og tekniske
+            rapporter med begrænset information er blevet normalen i
+            AI-industrien.
+          </li>
+          <li>
+            <strong
+              >Kan Generativ AI's popularitet oversættes til økonomisk
+              gevinst?</strong
+            >
+            Generative AI-produkter som ChatGPT oplever betydelig brugervækst,
+            men det er uklart, om indtægterne følger med. Driftsomkostningerne
+            for generative AI-produkter som ChatGPT er høje. Venturekapitalister
+            investerer i AI-selskaber i USA og UK, mens investeringer i EU
+            falder muligvis på grund af kommende AI-regulering.
+          </li>
+          <li>
+            <strong>Chipefterspørgslen og produktion stiger dramatisk.</strong>
+            Stigende investeringer i generativ AI øger behovet for GPU'er, som
+            er afgørende for træning af moderne AI-modeller. NVIDIAs aktiekurs
+            og omsætning er steget dramatisk på grund af stigende efterspørgsel
+            efter GPU'er. Flere lande er involveret i en global kamp om kontrol
+            over chipproduktion og eksport. Denne "chip-krig" er tydeligst
+            imellem, men ikke begrænset til, USA og Kina.
+          </li>
+          <li>
+            <strong
+              >Forskelligartede AI-Reguleringer bliver lige nu udarbejdet og
+              vedtaget over hele verden. </strong
+            >Lande som Indien og England vedtager en "light-touch" tilgang til
+            AI-regulering ved at bruge eksisterende lovgivning og kun indføre
+            nye love, hvor det er nødvendigt. Kina og EU arbejder på omfattende
+            AI-lovgivning baseret på samfunds- og risikoområder. USA og Canada
+            tager en hybridtilgang og vedtager forskellige love på
+            delstatsniveau og regulering i højrisikoindustrier.
+          </li>
+          <li>
+            <strong
+              >Sikkerhed og trusler Debatten om AI-bevidsthed og sikkerhed har
+              fået meget opmærksomhed, men der er stadig stor usikkerhed omkring
+              hvilke reelle trusler vi går i møde.</strong
+            >
+            Både politikere og eksperter tager spørgsmålet om AI-sikkerhed
+            alvorligt. En AI Safety Summit planlægges lige nu i London for at
+            diskutere sikkerhedsaspekterne ved AI og mødes med verdens ledere og
+            eksperter på området.
+          </li>
+        </ol>
+        <a href="https://www.stateof.ai" target="_blank"
+          >Hele rapporten kan findes her: https://www.stateof.ai</a
+        >
+      </div>
+      <div class="infobox">
+        <h2>Podcasts</h2>
+        <p>Gode podcasts, der altid eller ofte har AI som tema...</p>
+        <div class="podImgDiv">
+          <a href="https://aidenmark.dk/podcast" target="_blank">
+            <img
+              src="https://i.scdn.co/image/ab6765630000ba8a6f97c5ef1a41472db5de46eb"
+              class="podcastImg"
+              alt="https://aidenmark.dk/podcast"
+            />
+            AI Denmark
+          </a>
+        </div>
+        <div class="podImgDiv">
+          <a
+            href="https://www.dr.dk/lyd/special-radio/prompt-4712131124000"
+            target="_blank"
+          >
+            <img
+              src="https://api.dr.dk/radio/v2/images/urn:dr:radio:image:64d489401f6c4a4637238f43?ratio=16%3A9"
+              class="podcastImg"
+              alt="https://www.dr.dk/lyd/special-radio/prompt-4712131124000"
+            />
+            Prompt
+          </a>
+        </div>
+        <div class="podImgDiv">
+          <a href="https://lexfridman.com/podcast/" target="_blank">
+            <img
+              src="https://i.scdn.co/image/ab6765630000ba8a563ebb538d297875b10114b7"
+              class="podcastImg"
+              alt="https://lexfridman.com/podcast/"
+            />
+            Lex Fridman
+          </a>
+        </div>
+        <div class="podImgDiv">
+          <a href="https://www.humanetech.com/podcast" target="_blank">
+            <img
+              src="https://i.scdn.co/image/ab6765630000ba8a11849d1f22740968cb984b62"
+              class="podcastImg"
+              alt="https://www.humanetech.com/podcast"
+            />
+            Your undivided attention
+          </a>
+        </div>
+      </div>
     </div>
-
-    <ol>
-      <li>
-        <strong>Hvordan GPT-4 dominerer AI-landskabet.</strong>OpenAI's GPT-4
-        model, grundlaget for ChatGPT, har været i fokus i det seneste år. GPT-4
-        er anerkendt som den mest modne og kraftfulde store sprogmodel på
-        markedet. Den har demonstreret evnen til at bestå
-        universitetsniveau-eksaminer inden for fag som jura og medicin ud over
-        klassiske NLP-benchmarks.
-      </li>
-      <li>
-        <strong
-          >Hvordan RLHF har bevist sit værd og hvorfor det alligevel kritiseres
-          kraftigt.</strong
-        >
-        GPT-4 er trænet ved hjælp af "reinforcement learning from human
-        feedback" (RLHF), hvor menneskelige testere vurderer modelresultater.
-        RLHF er afgørende for præstationen af brugervenlige chatbots som
-        ChatGPT. Kritik er rejst angående de økonomiske og menneskelige
-        omkostninger ved RLHF og beskæftigelsen af mange testere./li>
-      </li>
-
-      <li>
-        <strong
-          >Hvordan en ny tidsånd af mindre vidensdeling og gennemsigtighed vandt
-          frem.</strong
-        >
-        Tidligere var offentliggørelse af forskningsresultater kutyme, men store
-        kommercielle AI-selskaber som OpenAI og Google er blevet mere lukkede.
-        Tekniske rapporter om ny AI-teknologi indeholder færre tekniske detaljer
-        og øger hemmeligholdelsen. Lukkethed og tekniske rapporter med begrænset
-        information er blevet normalen i AI-industrien.
-      </li>
-      <li>
-        <strong
-          >Kan Generativ AI's popularitet oversættes til økonomisk
-          gevinst?</strong
-        >
-        Generative AI-produkter som ChatGPT oplever betydelig brugervækst, men
-        det er uklart, om indtægterne følger med. Driftsomkostningerne for
-        generative AI-produkter som ChatGPT er høje. Venturekapitalister
-        investerer i AI-selskaber i USA og UK, mens investeringer i EU falder
-        muligvis på grund af kommende AI-regulering.
-      </li>
-      <li>
-        <strong>Chipefterspørgslen og produktion stiger dramatisk.</strong>
-        Stigende investeringer i generativ AI øger behovet for GPU'er, som er
-        afgørende for træning af moderne AI-modeller. NVIDIAs aktiekurs og
-        omsætning er steget dramatisk på grund af stigende efterspørgsel efter
-        GPU'er. Flere lande er involveret i en global kamp om kontrol over
-        chipproduktion og eksport. Denne "chip-krig" er tydeligst imellem, men
-        ikke begrænset til, USA og Kina.
-      </li>
-      <li>
-        <strong
-          >Forskelligartede AI-Reguleringer bliver lige nu udarbejdet og
-          vedtaget over hele verden. </strong
-        >Lande som Indien og England vedtager en "light-touch" tilgang til
-        AI-regulering ved at bruge eksisterende lovgivning og kun indføre nye
-        love, hvor det er nødvendigt. Kina og EU arbejder på omfattende
-        AI-lovgivning baseret på samfunds- og risikoområder. USA og Canada tager
-        en hybridtilgang og vedtager forskellige love på delstatsniveau og
-        regulering i højrisikoindustrier.
-      </li>
-      <li>
-        <strong
-          >Sikkerhed og trusler Debatten om AI-bevidsthed og sikkerhed har fået
-          meget opmærksomhed, men der er stadig stor usikkerhed omkring hvilke
-          reelle trusler vi går i møde.</strong
-        >
-        Både politikere og eksperter tager spørgsmålet om AI-sikkerhed
-        alvorligt. En AI Safety Summit planlægges lige nu i London for at
-        diskutere sikkerhedsaspekterne ved AI og mødes med verdens ledere og
-        eksperter på området.
-      </li>
-    </ol>
-    <a href="https://www.stateof.ai"
-      >Hele rapporten kan findes her: https://www.stateof.ai</a
-    >
   </div>
 </template>
 <script>
@@ -119,20 +273,57 @@ export default {
   background-color: white;
   border-radius: 8px;
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
-  display: flex;
-  flex-direction: column;
+  /* display: flex; */
+  /* flex-direction: column; */
   /* overflow: hidden; */
   font-family: "Roboto", sans-serif;
   padding: 2rem;
+}
+.innercontainer {
+  display: flex;
+  flex-direction: row;
 }
 .header {
   display: flex;
   align-items: center;
 }
+.infobox {
+  /* display: flex; */
+  /* align-items: center; */
+  margin-bottom: 2rem;
+  margin-top: 3rem;
+  border: 1px solid gray;
+  height: 22rem;
+  /* width: 25%; */
+  margin-left: auto;
+  /* float: right; */
+  padding: 1rem;
+}
+.contentbox {
+  display: flex;
+  flex-direction: column;
+  /* width: 70%; */
+}
 .headerLogo {
   width: 4rem;
   margin-right: 1rem;
 }
+.podcastImg {
+  height: 1rem;
+  width: 1rem;
+  margin-right: 1rem;
+}
+.imgDiv a {
+  display: flex;
+  align-items: center;
+  width: 50%;
+}
+.podImgDiv a {
+  display: flex;
+  align-items: center;
+  overflow: hidden;
+}
+
 li {
   margin-bottom: 1rem;
 }
@@ -140,10 +331,12 @@ h2 {
   margin-bottom: 0rem;
 }
 h3 {
+  margin-top: 3rem;
   margin-bottom: 0rem;
 }
 a {
   color: gray;
+  margin-top: 1rem;
 }
 a:visited {
   color: gray;
@@ -151,21 +344,41 @@ a:visited {
 a:hover {
   color: darkgray;
 }
+ol {
+  padding-inline-start: 1rem;
+}
 
 /* Medium devices (landscape tablets, 768px and up) */
 @media only screen and (min-width: 768px) {
   .newsImg {
-    padding: 1rem;
-    width: 50%;
+    padding: 1rem 0rem 1rem 0rem;
+    width: 100%;
   }
-  .imgDiv {
+  .infobox {
+    width: 25%;
+  }
+  .contentbox {
+    width: 70%;
+  }
+  /* .imgDiv {
     text-align: center;
-  }
+  } */
 }
 @media only screen and (max-width: 768px) {
   .newsImg {
     width: 100%;
     padding: 1rem;
+  }
+  .infobox {
+    width: 100%;
+  }
+  .contentbox {
+    width: 100%;
+  }
+  .innercontainer {
+    display: flex;
+    flex-direction: column;
+    margin-top: 3rem;
   }
 }
 </style>
